eac29d44b9e996e2b91a61f5e86d53537d112b0a
gelargew <gelargew@gmail.com>
2021-06-16 01:02:28 +0700

    drawer

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, NativeModules, Button } from 'react-native';
-import { TextForm } from './src/screens/FormsAndEvent';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Timer } from './src/screens/Timer';
 
 import { globalStateType } from './src/components/types';
+import { Dashboard } from './src/screens/Dashboard';
+import { TextForm } from './src/screens/FormsAndEvent';
 
-const { UIManager } = NativeModules
+const Drawer = createDrawerNavigator()
 export const globalState = React.createContext({} as globalStateType)
 
 export default function App() {
   const [darkTheme, setDarkTheme] = useState(true)
   return (
     <globalState.Provider value={{darkTheme, setDarkTheme}}>
-      <View style={styles.container}>
-        <Timer />
-      </View>
+      <NavigationContainer >      
+        <Drawer.Navigator openByDefault drawerType='permanent' drawerStyle={{width: 150, backgroundColor: '#222'}} initialRouteName='Timer'>
+            <Drawer.Screen name='Dashboard' component={Dashboard} />
+            <Drawer.Screen name='Timer' component={Timer} />
+            <Drawer.Screen name='Form' component={TextForm} />
+        </Drawer.Navigator>
+
+      </NavigationContainer>
     </globalState.Provider>
   );
 }
